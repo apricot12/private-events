fe82dc7084a2e3a8289dea04e2da7a0adc9e9fa2
Nihal Mohammed <mnihal64@outlook.com>
2021-03-23 21:55:40 +0530

    Fix redirect error after logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_login
+  
   def index
     @users = User.all
   end
@@ -8,5 +10,14 @@ class UsersController < ApplicationController
     @upcoming_events = current_user.attended_events.upcoming
     @attended_events = current_user.attended_events.past
     @created_events = current_user.created_events
+  end
+
+  private
+
+  def require_login
+    unless user_signed_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to events_path # halts request cycle
+    end
   end
 end
